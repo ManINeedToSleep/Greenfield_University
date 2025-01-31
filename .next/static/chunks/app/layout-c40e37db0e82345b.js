4328e656613ea13b9de3447b80f9285de8ed975b
ManINeedToSleep <bguna0050@launchpadphilly.org>
2025-01-31 13:39:35 -0500

    Delete .next directory

--- .next/static/chunks/app/layout-c40e37db0e82345b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7177],{451:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.t.bind(n,5786,23))},5786:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[4523,8173,8441,1517,7358],()=>s(451)),_N_E=e.O()}]);
